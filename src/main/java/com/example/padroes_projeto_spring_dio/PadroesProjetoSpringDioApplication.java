772b436689bf269fbfa483904fe7414a91d3aad6
JoseHenrique1 <jh4832184@gmail.com>
2025-03-18 08:39:53 -0300

    chore: add basic setup

---
+++ src/main/java/com/example/padroes_projeto_spring_dio/PadroesProjetoSpringDioApplication.java
@@ -0,0 +1,13 @@
+package com.example.padroes_projeto_spring_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSpringDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpringDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/padroes_projeto_spring_dio/PadroesProjetoSpringDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.padroes_projeto_spring_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
