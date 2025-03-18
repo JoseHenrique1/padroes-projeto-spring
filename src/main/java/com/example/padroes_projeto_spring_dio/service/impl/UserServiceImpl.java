b0956fa53a80e28e3df3c128735a08bb04984a45
JoseHenrique1 <jh4832184@gmail.com>
2025-03-18 09:55:39 -0300

    feat: add implementacao do crud

---
+++ src/main/java/com/example/padroes_projeto_spring_dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.padroes_projeto_spring_dio.service.impl;
+
+import com.example.padroes_projeto_spring_dio.model.User;
+import com.example.padroes_projeto_spring_dio.repository.UserRepository;
+import com.example.padroes_projeto_spring_dio.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    // Singleton: Injetar os componentes do Spring com @Autowired.
+    @Autowired
+    private UserRepository userRepository;
+
+    // Strategy: Implementar os métodos definidos na interface.
+    // Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
+
+    @Override
+    public Iterable<User> buscarTodos() {
+        // Buscar todos os Clientes.
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User buscarPorId(Long id) {
+        // Buscar Cliente por ID.
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    @Override
+    public void inserir(User user) {
+        salvarUser(user);
+    }
+
+    @Override
+    public void atualizar(Long id, User cliente) {
+        // Buscar Cliente por ID, caso exista:
+        Optional<User> userBd = userRepository.findById(id);
+        if (userBd.isPresent()) {
+            salvarUser(cliente);
+        }
+    }
+
+    @Override
+    public void deletar(Long id) {
+        // Deletar Cliente por ID.
+        userRepository.deleteById(id);
+    }
+
+    private void salvarUser(User user) {
+        userRepository.save(user);
+    }
+}
